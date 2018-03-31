650d1da28ea924a156bdba30968e4ea9ca32f7df
lousongtao <lousongtao@gmail.com>
2018-04-01 01:37:52 +0800

    don't use cache for category list, because if use cache, while category1 is too more over one screen, when slip the data up and down, some category1(not whole) cannot expand anymore.

--- app/src/main/java/com/shuishou/kitchen/ui/CategoryTabAdapter.java
+++ app/src/main/java/com/shuishou/kitchen/ui/CategoryTabAdapter.java
@@ -34,14 +34,16 @@ public class CategoryTabAdapter extends ArrayAdapter<Category1> {
     public View getView(final int position, View convertView, ViewGroup parent) {
         Category1 c1 = getItem(position);
 
-//        CategoryTabLayoutItem view = (CategoryTabLayoutItem)LayoutInflater.from(mainActivity).inflate(resourceId, null);
-//        view.init(mainActivity, c1);
-//        return view;
-        if (mapCategory1LayoutItem.get(c1.getId()) == null){
-            CategoryTabLayoutItem view = (CategoryTabLayoutItem)LayoutInflater.from(mainActivity).inflate(resourceId, null);
-            view.init(mainActivity, c1);
-            mapCategory1LayoutItem.put(c1.getId(), view);
-        }
-        return mapCategory1LayoutItem.get(c1.getId());
+        CategoryTabLayoutItem view = (CategoryTabLayoutItem)LayoutInflater.from(mainActivity).inflate(resourceId, null);
+        view.init(mainActivity, c1);
+        return view;
+
+        //TODO: 停止使用缓存对象, 在用缓存的过程中, 发现Category1数据过多, 超出屏幕限制后, 如果进行了上下滚动, 会有部分Category1(非全部)不能打开下面的二级菜单
+//        if (mapCategory1LayoutItem.get(c1.getId()) == null){
+//            CategoryTabLayoutItem view = (CategoryTabLayoutItem)LayoutInflater.from(mainActivity).inflate(resourceId, null);
+//            view.init(mainActivity, c1);
+//            mapCategory1LayoutItem.put(c1.getId(), view);
+//        }
+//        return mapCategory1LayoutItem.get(c1.getId());
     }
 }
